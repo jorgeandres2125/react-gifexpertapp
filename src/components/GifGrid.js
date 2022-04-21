259d965657a14376f6759de482d8723989675f8e
Jorge Andrés Torres Morales <jorgeandres2125@hotmail.com>
2022-04-20 19:54:40 -0500

    First Commit

---
+++ src/components/GifGrid.js
@@ -0,0 +1,52 @@
+//import React , { useState, useEffect } from 'react'
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+//import { getGifs } from '../helpers/getGifs';
+import { GifGridItem } from './GifGridItem';
+
+export const GifGrid = ( {category} ) => {
+
+    /*
+    const [ images, setImages ] = useState([]);
+    */
+   const { data:images, loading } = useFetchGifs( category );
+   /*useEffect(() => {
+            getGifs( category )
+            //.then( imgs => setImages (imgs));
+            .then( setImages );
+    }, [ category ])
+    */  
+
+
+
+    return (
+        <>
+        <h3 className='animate__animated animate__fadeIn'>{ category }</h3>
+        {loading && <p className='animate__animated animate__flash'>Loading Papu!</p>}
+        <div className="card-grid">            
+                {
+                    images.map( img  => (
+                        <GifGridItem 
+                            key = { img.id }
+                            { ...img }
+                        /> 
+                    ))
+                }
+            
+        </div>
+        {/*
+        <div className="card-grid">            
+                {
+                    images.map( img  => (
+                        <GifGridItem 
+                            key = { img.id }
+                            { ...img }
+                        /> 
+                    ))
+                }
+            
+        </div>
+        */}
+        </>
+    )
+}
