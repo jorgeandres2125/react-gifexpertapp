32a2846e7cdaa2cfede9a64bce270a5c14155077
Jorge Andrés Torres Morales <jorgeandres2125@hotmail.com>
2022-04-27 21:16:05 -0500

    Configuracion de test y modificacion de item

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import PropTypes  from 'prop-types';
 
-export const GifGridItem = ({id, title, url}) => {
+export const GifGridItem = ({ title, url}) => {
   return (
     <div className="card animate__animated animate__bounce animate__delay-2s animate__fadeIn">
         <img src ={ url } alt = { title }/>
         <p>{ title }</p>
     </div>
   )
+}
+
+GifGridItem.protoTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
 }
